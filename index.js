5572be3ec6a29d59b05edec1472948d0df0f32d6
adamtlee <adam.lee346@gmail.com>
2019-12-31 17:40:02 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+const port = (process.env.PORT || 3000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
+app.get('/', function(req, res) {
+    res.render('home');
+    res.
+});
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+app.get('/contact', function(req, res) {
+    res.render('contact');
+});
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`)
+});
+
+module.exports = app
